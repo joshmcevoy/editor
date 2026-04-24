3536cbd6c3e0c9e7cd63d7611afea1f0d59a7563
Josh <jmcevoyy@gmail.com>
2026-04-24 22:41:38 +1000

    Initial Commit

---
+++ editor/editorApp.swift
@@ -0,0 +1,17 @@
+//
+//  editorApp.swift
+//  editor
+//
+//  Created by Josh on 24/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct editorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
